7c19c7659631e3421fd639820845c8bd1f241972
Brandon Braun <brandonbraun653@gmail.com>
2018-01-15 20:28:35 -0600

    sync

--- rtx.hpp
+++ rtx.hpp
@@ -5,6 +5,15 @@
 
 #include <stdint.h>
 
+/* Rather than use an enum, this typedef approach is used to 
+ * force a strict size because this will be eventually transmitted
+ * over a bandwidth limited Wireless connection with XBEE */
+typedef uint8_t packetTypes;
+static const packetTypes PACKET_COMMAND		= 1;
+static const packetTypes PACKET_CONTROLS	= 2;
+static const packetTypes PACKET_REQUEST		= 4;
+
+
 struct inputReadings
 {
 	float SWR = 0.0;
@@ -24,11 +33,15 @@ struct parseResults
 	float YAW = 0.0;
 };
 
+#pragma pack(push,1)
 struct packetData
 {
+	packetTypes pktType = PACKET_CONTROLS;
 	float THROTTLE = 0.0;
 	float ROLL = 0.0;
 	float PITCH = 0.0;
 	float YAW = 0.0;
+	//Eventually add CRC calculations 
 };
+#pragma pack(pop)
 #endif
